c514b5ff607084fbfdda1a01015bb5469cf4658f
carloscruz85 <carloscruz85@gmail.com>
2021-07-05 13:51:40 -0600

    color and images updated

--- src/parts/myHeader.js
+++ src/parts/myHeader.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Mail from '../assets/media/mail.png'
+// import Mail from '../assets/media/mail.png'
 
 const MyHeader = () => {
+
+  const chatIcon = 'www.'+window.location.hostname+'/wp-content/plugins/cc85-chatbox/img/chat.png'
+
     return (
       <div className="my-chat-header">
-        <img className="my-chat-mail-image" src={Mail} alt="" />
+        <img className="my-chat-mail-image" src={chatIcon} alt="" />
       </div>
     )
   }
